675ed428495d312ed9ebd124a95595ba0a37ec06
0ishan0 <ishanbagchi4@gmail.com>
2020-05-11 16:41:20 +0000

    Finish the Blog app

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const methodOverride = require("method-override");
+const express          = require("express");
+const bodyParser       = require("body-parser");
+const mongoose         = require("mongoose");
+const methodOverride   = require("method-override");
+const expressSanitizer = require("express-sanitizer");
+const app              = express();
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost:27017/blogapp", 
@@ -11,6 +12,7 @@ mongoose.connect("mongodb://localhost:27017/blogapp",
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer()); //always goes after bodyParser
 app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
@@ -47,6 +49,7 @@ app.get("/blogs/new", function(req, res){
 // CREATE ROUTE
 app.post("/blogs", function(req, res){
 	//create blog
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.create(req.body.blog, function(err, newBlog){
 		if(err) {
 			console.log(err);
@@ -81,6 +84,7 @@ app.get("/blogs/:id/edit", function(req, res){
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
 		if(err) {
 			res.redirect("/blogs");
