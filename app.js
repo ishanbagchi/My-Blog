5272bc10faf1aa3b63339daddd7e57bbb60a593e
0ishan0 <ishanbagchi4@gmail.com>
2020-05-11 10:44:26 +0000

    Add NEW and CREATE routes

--- app.js
+++ app.js
@@ -26,12 +26,31 @@ app.get("/", function(req, res){
 	res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
 	Blog.find({}, function(err, blogs){
 		if(err){
 			console.log(err);
 		} else {
 			res.render("index", {blogs: blogs});
+		}
+	});
+});
+
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+})
+
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+	//create blog
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err) {
+			console.log(err);
+		} else {
+			//redirect to the index
+			res.redirect("/blogs");
 		}
 	});
 });
